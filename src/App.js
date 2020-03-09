750f1e6516026a1cc2d4238006c5a0a84f881593
Jeffrey Soriano <jeffsor17@gmail.com>
2020-03-09 17:05:24 -0600

    working project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import List from './List';
+import GroceryForm from './GroceryForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { groceries: [] }
+
+  getUniqId = () => {
+   return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+  }
+
+handleClick = (id) => {
+  const { groceries } = this.state;
+  this.setState({
+    groceries: groceries.map( grocery => {
+      if (grocery.id === id) {
+        return {
+          ...grocery,
+          complete: !grocery.complete
+        }
+      }
+      return grocery
+    })
+  })
 }
 
-export default App;
+addItem = (name) => {
+const { groceries } = this.state;
+const grocery = { name, id: this.getUniqId() , complete: false }
+this.setState({ groceries: [grocery, ...groceries] }); 
+}
+
+render() {
+const { groceries } = this.state;
+
+return (
+<div>
+<GroceryForm addItem={this.addItem} />
+<List name="Grocery List" items={groceries} groceryClick={this.handleClick} />
+</div>
+);
+}}
---
+++ src/GroceryForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class GroceryForm extends React.Component {
+    state = { name: '' }
+  
+    handleChange = (e) => {
+      this.setState({ name: e.target.value });
+    }
+  
+    handleSubmit = (e) => {
+      e.preventDefault();
+      console.log(this.state.name)
+      this.props.addItem(this.state.name);
+      this.setState({ name: '' })
+    }
+  
+    render() {
+      const { name } = this.state;
+  
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            value={name} 
+            name="name"
+            onChange={this.handleChange}
+            required 
+            placeholder="Add An Item" 
+          />
+        </form>
+      )
+    }
+  }
+  
+  export default GroceryForm;
